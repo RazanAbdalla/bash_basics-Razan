1c411ff4edaa84c2d16f02db70817b3b06ac6bb4
RazanAbdalla <zezo.qawasmeh@gmail.com>
2025-09-02 14:35:53 -0400

    Update 01-read_inputRazan.sh

--- 01-read_inputRazan.sh
+++ 01-read_inputRazan.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 # read the name of the user and print hello
 
-echo "Hello! What is your name"
+echo "Hello!My name is Razan Abdalla"
 read name
-echo "Welcome, $name"
+echo "Welcome, Razan!"
 
-# single quotes prevent the expansion of the variable
-echo 'Your name was stored in $name'
 
-# exercise: write a script that asks the user for a 
-# filename and create an empty file named after it
-
-echo "Hey $name, What is your filename?"
-read filename
-echo "You want $filename"
-echo "Creating $filename ..."
-touch $filename
-echo "$filename creted"
-ls
-echo "Bye,bye"
