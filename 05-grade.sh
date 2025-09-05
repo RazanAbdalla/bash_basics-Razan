753299071bc082fe5c29752bf786ad0c5889e8ed
Razan <rabdalla@ggc.edu>
2025-09-05 10:42:04 -0400

    Added temperature/grade messages for Part I exercise

--- 05-grade.sh
+++ 05-grade.sh
@@ -17,5 +17,23 @@ fi
 
 # exercise: write a script (that utilizes weather-util) 
 # that prints "it's cold" if the temperature is < 40
-# it's chilly if < 60, it's okay if < 70 and, it's hot for 
-# everything else
+# it's chilly if < 60, it's okay if < 70 and,it's hot for
+ # everything else
+
+#!/bin/bash
+
+# Ask the user for input
+echo "What is the current temperature?"
+echo "Please give a numeric answer"
+read temp
+
+# Check temperature and print message
+if [ "$temp" -lt 40 ]; then
+    echo "It's cold"
+elif [ "$temp" -lt 60 ]; then
+    echo "It's chilly"
+elif [ "$temp" -lt 70 ]; then
+    echo "It's okay"
+else
+    echo "It's hot"
+fi
